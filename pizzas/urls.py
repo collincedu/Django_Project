9687d6f9a3b97f75498ab1b580fe538c710fa768
Collin Covington <collin_covington1@baylor.edu>
2022-05-06 23:23:13 -0500

    pizzas link

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+import pizzas
 
 app_name = 'pizzas'
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('pizzas',views.pizzas, name='pizzas'),
 ]
